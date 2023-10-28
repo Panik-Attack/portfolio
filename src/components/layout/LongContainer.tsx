ce567ae95ad39f2f5069d2fa4a1f821ffb62fdbe
Eduardo J. Garcia Alfonso <eduardojga.work@gmail.com.com>
2023-10-27 23:59:14 -0400

    FIX: MainCOntainer> padding

--- src/components/layout/LongContainer.tsx
+++ src/components/layout/LongContainer.tsx
@@ -2,7 +2,7 @@ import { PropsContainer } from "../../types/types";
 
 export const LongContainer = ({ children }: PropsContainer) => {
   return (
-    <div className="p-5 relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden bg-[#af72ff56] hover:scale-[103%] transition duration-500 ease-in-out">
+    <div className="relative rounded-3xl col-span-2 md:aspect-auto overflow-hidden bg-[#af72ff56] hover:scale-[103%] transition duration-500 ease-in-out">
       {children}
     </div>
   );
